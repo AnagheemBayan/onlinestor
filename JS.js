f5b258909ab3afb2e562632e78b6e159bf5fe214
AnagheemBayan <anagheembayan411@gmail.com>
2021-01-28 16:40:15 +0200

    modified index and js files and delete java files

--- JS.js
+++ JS.js
@@ -1,5 +1,6 @@
 var pantstype = prompt ('welcome to pants page , what you want to shope , skinny or straight pants ') ;
 
+
  while (pantstype !== 'skinny' && pantstype !== 'straight' );{
 
     pantstype =  prompt('please write what you want correct  ') ;
@@ -17,30 +18,28 @@ for (var i = 0 ; i <pantinput ; i ++) {
 if(pantstype == "skinny"){
     document.write('<h3>' +'welcome to skinny pants page' +'</h3>');
 
-    img = img + '<img src= "https://n.nordstrommedia.com/id/sr3/31be938f-aba9-406c-ad5f-b5b2bb562c5f.jpeg">';
+    img =  '<img src= "https://n.nordstrommedia.com/id/sr3/31be938f-aba9-406c-ad5f-b5b2bb562c5f.jpeg">';
 
     document.write(img);
-
+ 
     
 
 }else if(pantstype == "straight"){
 
     document.write( '<h3>' +'welcome to straight pants page' + '</h3>');
 
-    img = img + '<img src= "https://images.jdmagicbox.com/quickquotes/images_main/b07fxmlky3-w-for-woman-women-s-straight-pants-18auw60193-110692-copper-foil-6-150915567-uqvhb.jpg">';
+    img = '<img src= "https://images.jdmagicbox.com/quickquotes/images_main/b07fxmlky3-w-for-woman-women-s-straight-pants-18auw60193-110692-copper-foil-6-150915567-uqvhb.jpg">';
 
     document.write(img) ;
 
 }
 
-else{
-    alert('No pants page are found !');
 
+
+//document.body.style.background= 'gray';
 }
 
 document.body.style.background= 'gray';
-}
-
 
 
 
